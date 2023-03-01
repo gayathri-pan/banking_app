ee8bc7d4417e6f50d406c5189df6b94f7c9c21da
Destiny Franks <desphixs@gmail.com>
2023-03-01 05:43:14 +0100

    Clean Commit - V1

---
+++ core/credit_card.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from core.models import CreditCard
+from account.models import Account
+from decimal import Decimal
+
+def all_cards(request):
+    account = Account.objects.get(user=request.user)
+    credit_card = CreditCard.objects.filter(user=request.user)
+
+    context = {
+        "account":account,
+        "credit_card":credit_card,
+    }
+    return render(request, "credit_card/all-card.html", context)
+
+def card_detail(request, card_id):
+    account = Account.objects.get(user=request.user)
+    credic_card = CreditCard.objects.get(card_id=card_id, user=request.user)
+
+    context = {
+        "account":account,
+        "credic_card":credic_card,
+    }
+    return render(request, "credit_card/card-detail.html", context)
+
+def withdraw_fund(request, card_id):
+    account = Account.objects.get(user=request.user)
+    credit_card = CreditCard.objects.get(card_id=card_id, user=request.user)
+
+    if request.method == "POST":
+        amount = request.POST.get("amount")
+        print(amount)
+
+        if credit_card.amount >= Decimal(amount):
+            account.account_balance += Decimal(amount)
+            account.save()
+
+            credit_card.amount -= Decimal(amount)
+            credit_card.save()
+
+            messages.success(request, "Withdrawal Successfull")
+            return redirect("core:card-detail", credit_card.card_id)
+        else:
+            messages.warning(request, "Insufficient Funds")
+            return redirect("core:card-detail", credit_card.card_id)
+
+
+def delete_card(request, card_id):
+    credit_card = CreditCard.objects.get(card_id=card_id, user=request.user)
+    credit_card.delete()
+    messages.success(request, "Card Deleted Successfull")
+    return redirect("account:dashboard")
